839c6c6f7190ad82c68ab6b6911652e7609dd83b
Matheus <matheus.b.peron@gmail.com>
2018-05-07 22:33:04 -0300

    Final Update

--- Examples/Functions_Demo.py
+++ Examples/Functions_Demo.py
@@ -17,17 +17,17 @@ PYl.CreateMasterFlat(obs)
 
 #-------diferentes opções da função que faz a redução completa.
 #faz a redução e combina as imagems pela mediana
-PYl.ReduceCompletely(obs,name = 'CUSTOM_NAME', combine_images = 1)
+# PYl.ReduceCompletely(obs,name = 'CUSTOM_NAME', combine_images = 1)
 
 #faz a redução e combina as imagems pela média
-PYl.ReduceCompletely(obs,name = 'CUSTOM_NAME', combine_images = 2)
+# PYl.ReduceCompletely(obs,name = 'CUSTOM_NAME', combine_images = 2)
 
 #mesma coisa, porém sem um nome de entrada (que por default será reduced)
-PYl.ReduceCompletely(obs, combine_images = 2)
+# PYl.ReduceCompletely(obs, combine_images = 2)
 
 # faz a redução das imagens sem combina-las.
 PYl.ReduceCompletely(obs,name = 'CUSTOM_NAME', combine_images = 0)
 
 #mesmo processo, porém mudando o nome e demonstrando que qualquer valor além de
 # combine_images =  1 e combine_images = 2 resulta na não combinação das imagens.
-PYl.ReduceCompletely(obs,name = 'OTHER_CUSTOM_NAME', combine_images = 25678)
+# PYl.ReduceCompletely(obs,name = 'OTHER_CUSTOM_NAME', combine_images = 25678)
